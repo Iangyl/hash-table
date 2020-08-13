9745f9e444e940776e483730c4be9ec3265f7a97
Iangyl <vohanchib@gmail.com>
2020-08-13 16:50:30 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/HashTable.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    class HashTable
+    {
+        private List<int>[] array = new List<int>[10];
+        public HashTable()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                array[i] = new List<int>();
+            }
+        }
+        private int HashCode(int value)
+        {         
+            string str = value.ToString();
+            if (str.Length > 1) return HashCount(value);
+            else return value;
+        }
+        private int HashCount(int value)
+        {
+            int result = 0;
+            while (value > 0)
+            {
+                int temp = value % 10;
+                result += temp;
+                value /= 10;
+            }
+            if (result > 9) return HashCount(result);
+            return result;
+        }
+        public void Insert(int value)
+        {
+            int id = HashCode(value);
+            array[id].Add(value);
+        }
+        public int[] Search(int value)
+        {
+            int id = HashCode(value);
+            var result = (from item in array[id]
+                         where item == value
+                         select item).ToArray();
+            return result;
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashTable table = new HashTable();
+            table.Insert(123);
+            table.Insert(321);
+            table.Insert(434);
+            var result = table.Search(434);
+            foreach (int item in result)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
